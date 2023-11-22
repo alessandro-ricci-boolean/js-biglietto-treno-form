507f7d03560796de815a503dc3af4d1beffa9d76
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-22 21:45:14 +0100

    added reset button

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 const button = document.getElementById("calc-price");
+const reset_button = document.getElementById("reset");
 
 button.addEventListener("click", function(){
     let name = document.getElementById("name").value;
@@ -6,13 +7,28 @@ button.addEventListener("click", function(){
     let age = document.getElementById("age").value;
     let price;
     if(age ==1){
-        price = 0.21 * km * 0.8;
+        price = (0.21 * km * 0.8).toFixed(2);
     }
-    else if(age ==2){
-        price = 0.21 * km;
+    else if(age ==3){
+        price = (0.21 * km).toFixed(2);
     }
     else{
-        price = 0.21 * km * 0.6;
+        price = (0.21 * km * 0.6).toFixed(2);
     }
     console.log(price)
+
+document.getElementById("name-surname").innerText = name;
+document.getElementById("km-inserted").innerText = km;
+document.getElementById("price").innerText = `${price.toFixed(2)}€`;
+
+});
+
+reset_button.addEventListener("click", function(){
+    document.getElementById("name").value="";
+    document.getElementById("km").value="";
+    document.getElementById("age").value="";
+
+    document.getElementById("name-surname").innerText = "";
+    document.getElementById("km-inserted").innerText = "";
+    document.getElementById("price").innerText = ``;
 });
