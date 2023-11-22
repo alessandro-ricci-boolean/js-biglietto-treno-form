c4e5f1743a590c482b1c23129731d0a8519a05f4
alessandro-ricci-boolean <cqfp.official@gmail.com>
2023-11-22 19:49:03 +0100

    added price functions

--- js/script.js
+++ js/script.js
@@ -1,8 +1,18 @@
 const button = document.getElementById("calc-price");
 
 button.addEventListener("click", function(){
-let name = document.getElementById("name").value;
-let km = document.getElementById("km").value;
-let age = document.getElementById("age").value;
-console.log(name,km,age);
-})
+    let name = document.getElementById("name").value;
+    let km = document.getElementById("km").value;
+    let age = document.getElementById("age").value;
+    let price;
+    if(age ==1){
+        price = 0.21 * km * 0.8;
+    }
+    else if(age ==2){
+        price = 0.21 * km;
+    }
+    else{
+        price = 0.21 * km * 0.6;
+    }
+    console.log(price)
+});
